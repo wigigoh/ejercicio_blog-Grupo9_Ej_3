6415ed77f1d6904325b7d461e6e5d7e1776ec920
Victoria <mvperera@gmail.com>
2023-05-04 13:00:52 -0300

    update proyecto base

--- ha_ejemplo_mvc_sequelize/routes/commentRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// Rutas relacionadas a los comentarios:
-// ...
-
-module.exports = router;
--- ha_ejemplo_mvc_sequelize/routes/publicRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-// Rutas relacionadas a la parte pública del sitio web:
-// ...
-
-module.exports = router;
