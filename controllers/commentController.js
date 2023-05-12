54a4507694d11256e8686293c8d52cb62f3499ac
FacundoRinon <facuzionff@gmail.com>
2023-05-12 16:37:02 -0300

    Comentarios

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -2,6 +2,13 @@ const { Comment } = require("../models");
 const { faker } = require("@faker-js/faker");
 
 async function store(req, res) {
+  // if (!req.body.userName) {
+  //   console.log("No hay Autor");
+  //   res.redirect("back");
+  // } else if (!req.body.Comment) {
+  //   console.log("No hay comentario");
+  //   res.redirect("back");
+  // }
   await Comment.create(
     {
       userName: req.body.userName,
